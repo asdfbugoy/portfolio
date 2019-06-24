f4f5e4c4c3e9ed60cf6b9737c46ccf6a4d952cdc
Francis Samande Declaro <>
2019-06-24 17:55:12 +0800

    added DailyIdea

---
+++ src/pages/DailyIdea.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const data = [
+    {
+        description: 'try studying MERN'
+    },
+    {
+        description: 'proper body placement in gym'
+    },
+    {
+        description: 'basketball proper training'
+    },
+    {
+        description: 'shoes check in 313'
+    },
+    {
+        description: 'check online clothes to wear and shoes'
+    },
+    {
+        description: 'passport renewal process check online'
+    }
+]
